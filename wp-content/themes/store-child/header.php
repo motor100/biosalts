576b5092d1ff784d4f3dbae4f73508710a40650e
Evgeny <260443@mail.ru>
2024-11-05 19:19:21 +0500

    Header html + css layout

--- wp-content/themes/store-child/header.php
+++ wp-content/themes/store-child/header.php
@@ -31,111 +31,86 @@
   <div id="page" class="hfeed site">
     <?php do_action('storefront_before_header'); ?>
 
-    <div class="header" id="myHeader">
-      <div class="main_menu">
-        <div class="container">
-          <div class="row">
-            <div class="head-nav">
-              <?php
-              wp_nav_menu(
-                array(
-                  'menu' => 'header_menu',
-                  'menu_id' => 'header_menu',
-                )
-              );
-              ?>
+    <header class="header">
+      <div class="header-desktop hidden-mobile">
+        <div class="header-top">
+          <div class="container">
+            <div class="flex-container">
+              <div class="header-top__address">
+                <img src="/wp-content/themes/store-child/includes/images/svg/geolocation.svg" class="geolocation" alt="геометка">
+                <span class="address-text">Московская область, совхоз им Ленина, Техцентр | Пн-Пт: 10:00 до 20:00, Сб-Вс: выходной</span>
+              </div>
+              <div class="socials-icons">
+                <a href="https://t.me/naturaproff" class="social-icons__link" rel="nofollow noopener" target="_blank">
+                  <img src="/wp-content/themes/store-child/includes/images/svg/circle-tg.svg" class="social-icons__image social-icons__tg" alt="телеграм">
+                </a>
+                <a href="https://vk.com/natura.pharma" class="social-icons__link" rel="nofollow noopener" target="_blank">
+                  <img src="/wp-content/themes/store-child/includes/images/svg/circle-vk.svg" class="social-icons__image social-icons__vk" alt="вконтакте">
+                </a>
+              </div>
+              <div class="phone-wrapper">
+                <a href="#" class="social-icons__link" rel="nofollow noopener" target="_blank">
+                  <img src="/wp-content/themes/store-child/includes/images/svg/circle-wa.svg" class="social-icons__image social-icons__wa" alt="ватсап">
+                </a>
+                <a href="tel:+74959274928" class="social-icons__link" rel="nofollow noopener" target="_blank">
+                  <img src="/wp-content/themes/store-child/includes/images/svg/circle-call.svg" class="social-icons__image social-icons__call" alt="звонок">
+                </a>
+                <a href="tel:+74959274928" class="phone-text">+7 (495) 927-4-928</a>
+              </div>
             </div>
+          </div>
+        </div>
+        <div class="header-content">
+          <div class="container">
+            <div class="flex-container">
+              <div class="header-logo">
+                <a href="/" class="header-logo__link">
+                  <img src="/wp-content/themes/store-child/includes/images/logo_header.png" alt="лого">
+                  <div class="underlogo-text">since 1873</div>
+                </a>
+              </div>
+              <a href="/catalog" class="catalog-btn header-content-btn">
+                <img src="/wp-content/themes/store-child/includes/images/svg/catalog-rectangle.svg" class="catalog-btn__image" alt="">
+                <span class="catalog-btn__text">Каталог</span>
+              </a>
+              <a href="/salts_questionnaire" class="select-salt-btn header-content-btn">
+                <img src="/wp-content/themes/store-child/includes/images/svg/magic-wand.svg" class="catalog-btn__image" alt="">
+                <span class="catalog-btn__text">Подбор солей</span>
+              </a>
+              <div class="header-search">
+                <?php echo do_shortcode('[fibosearch]'); ?>
+              </div>
+              <div class="header-cart">
+                <img src="/wp-content/themes/store-child/includes/images/svg/basket.svg" class="header-cart__basket" alt="корзина">
+                <span class="header-cart__counter">0</span>
+                <a href="/cart" class="full-link"></a>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="header-nav">
+          <div class="container">
+            <?php
+            wp_nav_menu(
+              array(
+                'menu' => 'header_menu',
+                'menu_id' => 'header_menu',
+              )
+            );
+            ?>
+          </div>
+        </div>
+        <div class="header-horizontal-line">
+          <div class="container">
+            <div class="horizontal-line"></div>
           </div>
         </div>
       </div>
-      <header class="main-header">
-        <div class="container">
-          <div class="row">
-            <div class="main-top">
-              <div class="align-self-center">
-                <div class="logo">
-                  <?php
-                  if (is_front_page()) :
-                  ?>
-                    <img src="<?php echo get_stylesheet_directory_uri(); ?>/includes/images/logo_header.png" alt="NaturaPharma">
-                  <?php
-                  else :
-                  ?>
-                    <a href="<?php echo esc_url(home_url('/')); ?>" rel="home">
-                      <img src="<?php echo get_stylesheet_directory_uri(); ?>/includes/images/logo_header.png" alt="NaturaPharma">
-                    </a>
-                  <?php
-                  endif;
-                  ?>
-                </div>
-              </div>
-              
-              <div class="align-self-center with-links">
-                <div class="header-links">
-                  <a href="https://vk.com/natura.pharma" target="_blank" rel="nofollow noopener">
-                    <div class="circle-blue vk"></div>
-                  </a>
-                  <a href="https://t.me/naturaproff" target="_blank" rel="nofollow noopener">
-                    <div class="circle-blue tg"></div>
-                  </a>
-                  <a href="tel:+74959274928">
-                    <div class="circle-blue phone"></div>
-                  </a>
-                  <div>
-                    <a href="tel:+74959274928" style="font-size: 18px;">+7 (495) 927-4-928</a>
-                  </div>
-                </div>
 
-                <?php echo do_shortcode('[fibosearch]'); ?>
-              </div>
-				
-              <div class="align-self-center with-menu">
-                <div class="header-navigation">
-                  <?php global $woocommerce; ?>
-
-                  <div class="woo-customs">
-                    <div>
-                        <a href="<?php echo $woocommerce->cart->get_cart_url() ?>">
-                            <div class="basket"></div>
-                            <div class="count">
-                                <?php echo sprintf($woocommerce->cart->cart_contents_count); ?>
-                            </div>
-                        </a>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </header>
-    </div>
+    </header>
 
     <div id="content" class="site-content" tabindex="-1">
-      <script>
-        if (window.innerWidth >= 991) {
-          window.onscroll = function() {
-            myFunction()
-          };
 
-          var header = document.getElementById("myHeader");
-          var sticky = header.offsetTop;
-
-          // Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
-          function myFunction() {
-            if (window.pageYOffset > sticky) {
-              header.classList.add("sticky");
-            } else {
-              header.classList.remove("sticky");
-            }
-		  }
-        }
-      </script>
-
-      <? if (!is_front_page()) { ?>
-        <div class="h_green_line">
-        </div>
-      <? } ?>
       <div class="container">
         <div class="row">
           <?php
